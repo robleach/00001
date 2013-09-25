5efa04423102850aed28106f575ed81ba6ea89b3
Rob Leach <robleachjax@gmail.com>
2013-09-24 22:37:01 -0400

    Create Example2.java

---
+++ Example2.java
@@ -0,0 +1,16 @@
+/* New food for flash */
+
+class Example2 {
+public static void main(String args[]) {
+int num;
+
+num = 100;
+
+System.out.println("This is num: " + num);
+
+num = num * 2;
+
+System.out.print("The value of num * 2 is ");
+SYstem.out.println(num);
+}
+}
